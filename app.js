e5888a8ff46ba0b31be24b9d75b8a0d9f2fba2d4
Zabih Keraam <keraamzabih@gmail.com>
2025-03-26 15:30:23 +0430

    New route added

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    res.send("Hello from App runner!");
-});
+const cors = require('cors')
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }))
+app.get('/', (req, res)=>{
+    res.send("congratulation you are connected to Express backend!")
+})
+app.get('/api', (req, res)=>{
+    res.status(200).json({status: "Success", message: "If you see this message, congratulation you received this message from Express backend!"})
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
